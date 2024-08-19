5dcd6753d353dcd67dc89257618f6872160bdce9
Joseph Lennox <jlennox@gmail.com>
2024-08-19 12:33:09 -0700

    Add yet to be used test and common projects.

---
+++ src/z1.Tests/ActorTests.cs
@@ -0,0 +1,24 @@
+﻿using z1.Actors;
+
+namespace z1.Tests;
+
+internal static class TestObjects
+{
+    public static Game Game => new();
+}
+
+internal class ActorTests
+{
+    [Test]
+    [TestCase(ObjType.Zora)]
+    [TestCase(ObjType.BlueWizzrobe)]
+    [TestCase(ObjType.RedWizzrobe)]
+    [TestCase(ObjType.PatraChild1)]
+    [TestCase(ObjType.Wallmaster)]
+    [TestCase(ObjType.Ganon)]
+    public void EnsureProperObjTimer(ObjType type)
+    {
+        var actor = Actor.FromType(type, TestObjects.Game, 0, 0);
+        Assert.That(actor.ObjTimer, Is.EqualTo(0));
+    }
+}
